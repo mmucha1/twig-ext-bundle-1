56e7ab6ff2df26e0e4c3ba3cebd5d8170daa8f7a
Maciej Mucha <mmucha@ioki.com.pl>
2013-03-13 14:55:01 +0100

    star func

--- Twig/FunnyExtension.php
+++ Twig/FunnyExtension.php
@@ -21,12 +21,18 @@ class FunnyExtension extends \Twig_Extension
     {
         return array(
             'smile' => new \Twig_Function_Method($this, 'smileFunction'),
+            'stars' => new \Twig_Function_Method($this, 'starsFunction'),
         );
     }
 
     public function smileFunction($number = 5)
     {
         return str_repeat(':-)', $number);
+    }
+
+    public function starsFunction($number = 3)
+    {
+        return str_repeat('*', $number);
     }
 
     public function getName()
